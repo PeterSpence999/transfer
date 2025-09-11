66b5b71a96fce4269b95e5e6e0db0c99a4519044
PeterSpence999 <61089718+PeterSpence999@users.noreply.github.com>
2025-09-11 14:43:26 +0100

    Add files via upload

--- ODE_PJS.py
+++ ODE_PJS.py
@@ -7,60 +7,59 @@ def ode_analytic(x,a,fa,D,omega):
 #********************************************************
 #Set domain
 #********************************************************
-a=1
-b=2
+a=1.0
+b=2.0
 #********************************************************
 #Set boundary conditions
 #********************************************************
-fa=1
-D=10
-omega=10
-n=range(100,1000,100)
-dx=np.zeros(len(n))
-err=np.zeros(len(n))
+fa=1.0
+D=10.0
+omega=10.0
+n=range(11,101,10)
+nN=np.size(n)
+dx=np.zeros(nN)
+err=np.zeros(nN)
 #********************************************************
 #Cycle through discretisations
 #********************************************************
-for j in range(0,len(n)):
+for j in range(0,nN):
     #initialise solution and discretisation
-    dx[j]=(b-a)/n[j]
-    xi=np.arange(a,b,dx[j])
+    #dx[j]=(b-a)/(n[j]-1)
+    #xi=np.arange(a,b,dx[j])
+    xi,dx[j]=np.linspace(a,b,num=n[j],retstep=True)
     #Calculate numerical solution
-    f_num=np.zeros(n[j]) #initialise numerical vector
-    f_num[0]=fa
+    fnum=np.zeros(n[j]) #initialise numerical vector
+    fnum[0]=fa
     for i in range(0,n[j]-1):
         if i==0:
-            f_num[i+1]=0.5*fa*(2.0+omega*dx[j]**2)+D*dx[j]
+            fnum[i+1]=0.5*fa*(2.0+omega*dx[j]**2)+D*dx[j]
         elif i>0:
-            f_num[i+1]=f_num[i]*(2.0+omega*dx[j]**2)-f_num[i-1]
+            fnum[i+1]=fnum[i]*(2.0+omega*dx[j]**2)-fnum[i-1]
     #Calculate analytic solution
-    f_an=ode_analytic(xi,a,fa,D,omega)
-    err[j]=max(np.abs(f_num-f_an))
-    if j==len(n)-1:
+    fan=ode_analytic(xi,a,fa,D,omega)
+    err[j]=max(np.abs(fnum-fan))
+    if j==0:
         #Display solution
         plt.figure(1)
-        plt.plot(xi,f_an,'b')
-        plt.plot(xi,f_num,'ro')
+        plt.plot(xi,fan,'b')
+        plt.plot(xi,fnum,'ro')
         plt.xlabel('x')
         plt.ylabel('f(x)')
         plt.grid()
 #********************************************************
 #calculate order
 #********************************************************
-em=err[len(n)-1]
-dxm=dx[len(n)-1]
-p=np.zeros(len(n)-2)
-for i in range(0,len(n)-2):
-    p[i]=np.log(err[i]/em)/np.log(dx[i]/dxm)
+em=err[nN-1]
+dxm=dx[nN-1]
+p=np.zeros(nN-1)
+for i in range(0,nN-1):
+    p[i]=np.log(em/err[i])/np.log(dxm/dx[i])
 #********************************************************
 #Display order with number of nodes
 #********************************************************
 plt.figure(2)
-plt.plot(n[0:len(n)-2],p)
+plt.plot(n[0:nN-1],p)
 plt.xlabel('Number of nodes, n')
 plt.ylabel('Calculated order, p')
 plt.grid()
 plt.show()
-
-
-
